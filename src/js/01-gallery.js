07e7b00fe13a9fda9b1a6f637297bf551746d36f
andriytol <andriyytolstoy@gmail.com>
2023-03-05 21:30:25 +0200

    finished the taskі

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+function createGalleryItemsMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+    return `
+    <a class="gallery__item" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+    />
+    </a>`}).join('');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const LOCAL_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+populateFeedbackForm();
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onInputData, 500));
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    const { email, message } = e.currentTarget.elements;
+    console.log({ email: email.value, message: message.value });
+    localStorage.removeItem(LOCAL_KEY);
+    e.currentTarget.reset();
+};
+
+function onInputData() {
+    let data = localStorage.getItem(LOCAL_KEY);
+    data = data ? JSON.parse(data) : {};
+    let { email, message } = form.elements;
+    data = {
+    email: email.value.trim(),
+    message: message.value.trim(),
+    };
+
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(data));
+};
+
+function populateFeedbackForm() {
+    let data = localStorage.getItem(LOCAL_KEY);
+    if (data) {
+    data = JSON.parse(data);
+    Object.entries(data).forEach(([name, value]) => {
+        form.elements[name].value = value ?? '';
+        });
+    };
+};
